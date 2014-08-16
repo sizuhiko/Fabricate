b2bc343a8d07c042655b79cd65fa69576d421cbb
sizuhiko <k-kishida@esm.co.jp>
2014-08-16 21:12:26 +0900

    change phpcs installed_path

--- install_cakephp_codesniffer.sh
+++ install_cakephp_codesniffer.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 if [ "$PHPCS" = '1' ]; then
-	git clone -b master https://github.com/cakephp/cakephp-codesniffer.git ../cakephp-codesniffer
-	phpcs --config-set installed_paths ../cakephp-codesniffer
+	git clone -b master https://github.com/cakephp/cakephp-codesniffer.git $HOME/cakephp-codesniffer
+	phpcs --config-set installed_paths $HOME/cakephp-codesniffer
 	export PHPCS=1
 	exit 0
 fi
