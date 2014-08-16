82fba336b7a71486c0df914ff2f3ee4c782b7d30
sizuhiko <k-kishida@esm.co.jp>
2014-08-16 20:08:41 +0900

    install phpcs by before_script

--- before_script.sh
+++ before_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ "$PHPCS" = '1' ]; then
-	pear channel-discover pear.cakephp.org
+	pear install PHP_CodeSniffer	
 	git clone -b master https://github.com/cakephp/cakephp-codesniffer.git ../cakephp-codesniffer
 	phpcs --config-set installed_paths ../cakephp-codesniffer
 	exit 0
