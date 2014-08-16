0fd90d7088ee7eeb776339b608ca3698d33e84eb
sizuhiko <k-kishida@esm.co.jp>
2014-08-16 20:30:51 +0900

    phpenv rehash after install pear

--- before_script.sh
+++ before_script.sh
@@ -3,6 +3,7 @@
 if [ "$PHPCS" = '2' ]; then
 	pear install PHP_CodeSniffer	
 	git clone -b master https://github.com/cakephp/cakephp-codesniffer.git ../cakephp-codesniffer
+	phpenv rehash
 	phpcs --config-set installed_paths ../cakephp-codesniffer
 	export PHPCS=1
 	exit 0
