2ed72c9ee055662f7461474b673f6d7d90dc1cec
sizuhiko <k-kishida@esm.co.jp>
2014-08-16 20:23:53 +0900

    PHPCS 2 use newest code sniffer

--- before_script.sh
+++ before_script.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-if [ "$PHPCS" = '1' ]; then
+if [ "$PHPCS" = '2' ]; then
 	pear install PHP_CodeSniffer	
 	git clone -b master https://github.com/cakephp/cakephp-codesniffer.git ../cakephp-codesniffer
 	phpcs --config-set installed_paths ../cakephp-codesniffer
+	export PHPCS=1
 	exit 0
 fi
